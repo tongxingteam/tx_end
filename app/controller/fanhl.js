e315eca1b4ed59dc1a54a97fd20a3c697cc27370
FanHaiLiang <18342964755@163.com>
2018-06-05 16:41:47 +0800

    拒绝接口

--- app/controller/fanhl.js
+++ app/controller/fanhl.js
@@ -5,7 +5,29 @@ const Controller = require('egg').Controller;
 class FanhlController extends Controller {
   // 发布人不同意参团
   async notAgreeJoin(){
-
+    const { fanhl } = this.ctx.service;
+    let { apply_trip_id, user_id, apply_publisher_id } = this.ctx.request.body;
+    if(!apply_trip_id || !user_id || !apply_publisher_id){
+      this.ctx.status = 400;
+      this.ctx.body = {code:40000,msg:"参数错误"}
+    }else{
+      try{
+        let changeStatus = await fanhl.changeApplyStatus(apply_trip_id,user_id,apply_publisher_id);
+        //拒绝成功
+        if(changeStatus === 1){
+          this.ctx.status = 200;
+          this.ctx.body = {code:20000,msg:'拒绝成功'}
+        }else{
+          //未知错误导致的拒绝失败
+          this.ctx.status = 500;
+          this.ctx.body = {code:50000,msg:"服务器错误"}
+        }
+      }catch(error){
+        console.log(error)
+        this.ctx.status = 500;
+        this.ctx.body = {code:50000,msg:"服务器错误"}
+      }
+    }
   }
   // 退出行程团
   async leave(){
--- app/service/fanhl.js
+++ app/service/fanhl.js
@@ -3,7 +3,23 @@
 const Service = require('egg').Service;
 
 class FanhlService extends Service {
-    
+    //根据用户id,行程id,发起人id修改用户的申请状态
+    async changeApplyStatus(apply_trip_id, user_id, apply_publisher_id) {
+        const { APPLY_DB } = this.config.mysql;
+        const { mysql } = this.app;
+        let changeStatus = await mysql.update(APPLY_DB, 
+            {
+                apply_status_to_add:2,
+                 apply_status_to_user:1
+            },{
+                where: {
+                    apply_trip_id, 
+                    user_id, 
+                    apply_publisher_id,
+                }
+            })
+        return changeStatus.affectedRows;
+    }
 }
 
 module.exports = FanhlService;
