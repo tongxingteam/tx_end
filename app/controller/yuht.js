0d973233dd480964fee8e3242257b93ebc6d116b
bug100000 <bug100000@outlook.com>
2018-06-09 17:55:12 +0800

    修改：     app/controller/yuht.js 	修改：     app/service/yuht.js 	新文件：   readme_y.md 	初步开发完我发起的行程、我参与的行程、我的草稿行程接口

--- app/controller/yuht.js
+++ app/controller/yuht.js
@@ -3,17 +3,71 @@
 const Controller = require('egg').Controller;
 
 class YuhtController extends Controller {
-  // 用户发布的行程
-  async queryMyPublish(){
+   // 用户发布的行程
+   async queryMyPublish(){
+    // 获取service
+    const { yuht } = this.ctx.service;
+    // 获取请求参数
+    const { user_id, currentPage, pageSize } = this.ctx.request.body;
+    // 判断参数是否合法
+    // console.log(user_id, currentPage, pageSize, typeof(pageSize)=="number", !typeof(pageSize)=="number");
+    if(!user_id || !currentPage || !pageSize){
+      console.log("参数不合法");
+      return this.ctx.body = {code: 40001, msg: '参数不合法'};
+    }
 
+    try{
+      // 等待数据库返回结果
+      const trip = await yuht.queryMyTripList(user_id, currentPage, pageSize);
+      this.ctx.body = trip;
+    }catch(error){
+      console.log(error);
+      this.ctx.status = 500;
+      this.ctx.body = {code: 50000, msg: '服务器错误'};
+    }
   }
+  
   // 用户参与的同行
   async queryMyTx(){
-
+    // 获取service
+    const { yuht } = this.ctx.service;
+    // 获取请求参数
+    const { user_id, currentPage, pageSize } = this.ctx.request.body;
+    // 判断参数是否合法
+    if(!user_id || !currentPage || !pageSize){
+      console.log("参数不合法");
+      return this.ctx.body = {code: 40001, msg: '参数不合法'};
+    }
+    try{
+      // 等待数据库返回结果
+      const trip = await yuht.queryJoinTripList(user_id, currentPage, pageSize);
+      this.ctx.body = trip;
+    }catch(error){
+      console.log(error);
+      this.ctx.status = 500;
+      this.ctx.body = {code: 50000, msg: '服务器错误'};
+    }
   }
   // 我的草稿
   async queryMyDraftList(){
-
+    // 获取service
+    const { yuht } = this.ctx.service;
+    // 获取请求参数
+    const { user_id, currentPage, pageSize } = this.ctx.request.body;
+    // 判断参数是否合法
+    if(!user_id || !currentPage || !pageSize){
+      console.log("参数不合法");
+      return this.ctx.body = {code: 40001, msg: '参数不合法'};
+    }
+    try{
+      // 等待数据库返回结果
+      const trip = await yuht.queryMyDraftTripList(user_id, currentPage, pageSize);
+      this.ctx.body = trip;
+    }catch(error){
+      console.log(error);
+      this.ctx.status = 500;
+      this.ctx.body = {code: 50000, msg: '服务器错误'};
+    }
   }
   // 我的申请页面
   async queryMyRequestList(){
--- app/service/yuht.js
+++ app/service/yuht.js
@@ -3,7 +3,108 @@
 const Service = require('egg').Service;
 
 class YuhtService extends Service {
-    
+    // 查询我发起的行程列表
+    async queryMyTripList(user_id, currentPage, pageSize) {
+        const {
+            TRIP_DB
+        } = this.config.mysql;
+        const {
+            mysql
+        } = this.app;
+        // sql
+        // return await mysql.select(TRIP_DB, { // 搜索 post 表
+        //     where: {
+        //         "publish_user_id": user_id
+        //     }, // WHERE 条件
+        //     columns: ['trip_create_time', 'trip_end_location', 'trip_start_time', 'trip_end_time', 'trip_member_count', 'trip_id', 'trip_status', 'trip_member_info'], // 要查询的表字段
+        //     orders: [
+        //         ['trip_create_time', 'desc']
+        //     ], // 排序方式
+        //     limit: pageSize, // 返回数据量
+        //     offset: (currentPage - 1) * pageSize, // 数据偏移量
+        // });
+        const trip = await mysql.query(`
+        select SQL_CALC_FOUND_ROWS trip_create_time,trip_end_location,trip_start_time,trip_end_time,trip_member_count,trip_id,trip_status,trip_member_info 
+        from ${TRIP_DB}
+        where publish_user_id='${user_id}'
+        order by trip_create_time desc
+        limit ${(currentPage - 1) * pageSize}, ${pageSize};
+        `);
+        const total = await mysql.query(`select found_rows()`);
+        return {
+            "total": total[0]["found_rows()"],
+            trip
+        }
+    }
+
+    // 查询我参与的行程列表
+    async queryJoinTripList(user_id, currentPage, pageSize) {
+        const {
+            TRIP_DB
+        } = this.config.mysql;
+        const {
+            mysql
+        } = this.app;
+        // sql
+        const trip = await mysql.query(`
+        select SQL_CALC_FOUND_ROWS trip_create_time,trip_end_location,trip_start_time,trip_end_time,trip_member_count,trip_id,trip_status,trip_member_info 
+        from ${TRIP_DB}
+        where trip_member_info like '%${user_id}%'
+        order by trip_create_time desc
+        limit ${(currentPage - 1) * pageSize}, ${pageSize};
+        `);
+        const total = await mysql.query(`select found_rows()`);
+        return {
+            "total": total[0]["found_rows()"],
+            trip
+        }
+    }
+
+    // 查询我的草稿
+    async queryMyDraftTripList(user_id, currentPage, pageSize) {
+        const {
+            TRIP_DB
+        } = this.config.mysql;
+        const {
+            mysql
+        } = this.app;
+        // sql
+        const trip = await mysql.query(`
+            select SQL_CALC_FOUND_ROWS trip_create_time,trip_start_location,trip_end_location,trip_start_time,trip_end_time,trip_member_count,trip_id,trip_other_desc
+            from ${TRIP_DB}
+            where publish_user_id='${user_id}' and trip_status=0
+            order by trip_create_time desc
+            limit ${(currentPage - 1) * pageSize}, ${pageSize};
+            `);
+        const total = await mysql.query(`select found_rows()`);
+        return {
+            "total": total[0]["found_rows()"],
+            trip
+        }
+    }
+
+    // 查询我的申请记录
+    // async queryMyApplyTripList(user_id, currentPage, pageSize) {
+    //     const {
+    //         TRIP_DB
+    //     } = this.config.mysql;
+    //     const {
+    //         mysql
+    //     } = this.app;
+    //     // sql
+    //     const trip = await mysql.query(`
+    //             select SQL_CALC_FOUND_ROWS trip_create_time,trip_start_location,trip_end_location,trip_start_time,trip_end_time,trip_member_count,trip_id,trip_other_desc
+    //             from ${TRIP_DB}
+    //             where publish_user_id='${user_id}' and trip_status=0
+    //             order by trip_create_time desc
+    //             limit ${(currentPage - 1) * pageSize}, ${pageSize};
+    //             `);
+    //     const total = await mysql.query(`select found_rows()`);
+    //     return {
+    //         "total": total[0]["found_rows()"],
+    //         trip
+    //     }
+    // }
 }
 
 module.exports = YuhtService;
